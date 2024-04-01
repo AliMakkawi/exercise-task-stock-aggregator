a30d31626661afa61d448741f6fed8e390eaa486
Ali Makkawi <ali.makkawi@dim.construction>
2024-04-01 08:33:31 +0300

    add function to get all prices for a stock

--- app/StockMarket/Infrastructure/Repositories/EloquentStockRepository.php
+++ app/StockMarket/Infrastructure/Repositories/EloquentStockRepository.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\StockMarket\Infrastructure\Repositories;
 
+use App\StockMarket\Domain\Models\Stock;
+
 final class EloquentStockRepository implements StockRepository
 {
+    private function findAllPricesForStock(Stock $stock): Collection
+    {
+        return $stock->prices()->latest('date_and_time_of_price')->get();
+    }
+
     private function findPercentageChangeForStock(string $currentPrice, string $previousPrice): ?string
     {
         if(bccomp($previousPrice, '0', 4) === 0) {
