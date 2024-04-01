31d879dab668dcca64c53a2859dac16c3496ff32
Ali Makkawi <ali.makkawi@dim.construction>
2024-04-01 08:32:10 +0300

    added private function to calculate percentage change

--- app/StockMarket/Infrastructure/Repositories/EloquentStockRepository.php
+++ app/StockMarket/Infrastructure/Repositories/EloquentStockRepository.php
@@ -6,4 +6,14 @@ namespace App\StockMarket\Infrastructure\Repositories;
 
 final class EloquentStockRepository implements StockRepository
 {
+    private function findPercentageChangeForStock(string $currentPrice, string $previousPrice): ?string
+    {
+        if(bccomp($previousPrice, '0', 4) === 0) {
+            return null;
+        }
+
+        $difference = bcsub($currentPrice, $previousPrice, 4);
+
+        return bcmul(bcdiv($difference, $previousPrice, 6), '100', 4);
+    }
 }
