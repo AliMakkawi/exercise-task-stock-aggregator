c50dcc6781cfe22fa2e54415f40ace525a1a2200
Ali Makkawi <ali.makkawi@dim.construction>
2024-04-01 09:02:55 +0300

    add rate limit for all api routes

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\StockController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/users', [UserController::class, 'store']);
+Route::middleware(['throttle:api'])->group(function () {
+    Route::post('/users', [UserController::class, 'store']);
 
-
-Route::prefix('/stocks')->name('stocks.')->middleware(['auth:sanctum', 'abilities:fetch-stocks'])->group(function () {
-    Route::get('/', [StockController::class, 'index'])->name('index');
-    Route::get('query', [StockController::class, 'query'])->name('query');
-    Route::get('current', [StockController::class, 'current'])->name('current');
+    Route::prefix('/stocks')->name('stocks.')->middleware(['auth:sanctum', 'abilities:fetch-stocks'])->group(function () {
+        Route::get('/', [StockController::class, 'index'])->name('index');
+        Route::get('query', [StockController::class, 'query'])->name('query');
+        Route::get('current', [StockController::class, 'current'])->name('current');
+    });
 });
